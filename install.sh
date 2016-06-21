45f0a61cac355089722996afc6d9e150df6eacc4
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2016-06-20 22:35:26 -0300

    installation script

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Location of the cloned dotfiles repository
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Backup old files
+backup_dir=$HOME/.dotfiles_backup
+[ -d $backup_dir ] || mkdir $backup_dir || exit 1
+
+files_to_backup=(
+    .vimrc
+    .zshrc
+    .i3*
+    .dunstrc
+    .xinitrc
+    .Xresources
+)
+
+for file in $files_to_backup; do
+    mv $HOME/$file $backup_dir 2> /dev/null
+done
+
+# Copying new dotfiles
+files_to_copy=(
+    vimrc
+    zshrc
+    i3*
+    dunstrc
+    xinitrc
+    Xresources
+)
+
+for file in $files_to_copy; do
+    cp $DIR/$file $HOME/.$file -rv
+done
+
+echo "Done!"
+exit 0
